5d60fc5d3d24f28f1949aa9b73ff066a4cb7d676
Kate Haskell <fuzzcat@bactroid.net>
2016-09-08 17:51:25 -0400

    Module 1 solution with bonus credit

---
+++ module01-solution/app.js
@@ -0,0 +1,39 @@
+(function () {
+
+angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+    $scope.message = '';
+    $scope.lunchList = '';
+    $scope.inputClass = '';
+    $scope.msgClass = '';
+
+    $scope.evalList = function () {
+        $scope.lunchList = $scope.lunchList.trim();
+
+        // Check for empty list box
+        if ($scope.lunchList.length === 0) {
+            $scope.message = 'Please enter data first';
+	    $scope.msgClass = 'invalid_msg';
+	    $scope.inputClass = 'invalid_input';
+            return;
+        }
+
+        var list = $scope.lunchList.split(',');
+	$scope.msgClass = 'valid_msg';
+	$scope.inputClass = 'valid_msg';
+
+        if (list.length <= 3) {
+            $scope.message = 'Enjoy!';
+        }
+
+        else {
+            $scope.message = 'Too much!';
+        }
+    };
+}
+
+})();
