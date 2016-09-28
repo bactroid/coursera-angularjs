127bfe1343c3a24d91651d4e9014c84025ff6fe3
Kate Haskell <fuzzcat@bactroid.net>
2016-09-28 11:17:09 -0400

    Pulling items without isolate scope

---
+++ module03-solution/js/app.js
@@ -0,0 +1,51 @@
+(function () {
+angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service ('MenuSearchService', MenuSearchService)
+  .directive('foundItems', foundItems)
+  .constant('restUrl', 'https://davids-restaurant.herokuapp.com');
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController (MenuSearchService) {
+  var ctrl = this;
+  ctrl.foundItems = [];
+  ctrl.searchItems = function (searchTerm) {
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+    promise.then(function (response) {
+      ctrl.foundItems = response;
+    });
+  };
+}
+
+MenuSearchService.$inject = ['$http', 'restUrl'];
+function MenuSearchService ($http, restUrl) {
+  this.getMatchedMenuItems = function getMatchedMenuItems (searchTerm) {
+    return $http({
+      url: restUrl + '/menu_items.json'
+    })
+    .then (
+      function success (response) {
+        //var foundItems = response.data.menu_items;
+        var foundItems = [];
+        response.data.menu_items.forEach(function (element, index, array) {
+          if (element.description.search(searchTerm) !== -1) {
+            foundItems.push(element);
+          }
+        });
+        return foundItems;
+      },
+      function error (response) {
+        console.log("Error getting HTTP response.")
+      }
+    );
+  }
+}
+
+function foundItems () {
+  var ddo = {
+    templateUrl: 'templates/found-items.html'
+  };
+  return ddo;
+}
+
+})();
