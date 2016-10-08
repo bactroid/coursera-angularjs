8b408640fd312a0906ae315b8b63988a6ec33460
Kate Haskell <fuzzcat@bactroid.net>
2016-10-08 17:50:24 -0400

    Moved routes into separate file

--- module04-solution/src/js/menuapp.module.js
+++ module04-solution/src/js/menuapp.module.js
@@ -3,42 +3,4 @@
 
 angular.module('MenuApp', ['ui.router', 'data']);
 
-angular.module('MenuApp')
-    .config(RoutesConfig);
-
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesConfig ($stateProvider, $urlRouterProvider){
-    // Make "home" the default route
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'src/views/home.html'
-        })
-
-        .state('categories', {
-            url: '/categories',
-            templateUrl: 'src/views/categories.html'
-        })
-
-        .state('items', {
-            url: '/items/{category}',
-            templateUrl: 'src/views/items.html',
-            controller: 'ItemsComponentController as menu_items',
-            resolve: {
-                list: ['$stateParams',
-                       'MenuDataService',
-                       function ($stateParams, MenuDataService) {
-                             return MenuDataService.getItemsForCategory($stateParams.category)
-                                .then(function (response) {
-                                    console.log(response.menu_items);
-                                    return response.menu_items;
-                                });
-                         }
-                ]
-            }
-        });
-}
-
 })();
