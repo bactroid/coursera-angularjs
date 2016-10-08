30cb6bed9ebe3eb4399486fd241491c9aa1cc7f1
Kate Haskell <fuzzcat@bactroid.net>
2016-10-08 07:12:47 -0400

    Skeleton for MenuDataService added

---
+++ module04-solution/src/js/menudata.service.js
@@ -0,0 +1,12 @@
+(function () {
+angular.module('data')
+    .service('MenuDataService', MenuDataService);
+
+function MenuDataService () {
+    this.getAllCategories = function () {
+    };
+
+    this.getItemsForCategory = function (categoryShortName) {
+    };
+}
+})();
